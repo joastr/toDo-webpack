433b050920e02ba001cc5b059363668066f105dc
Joanna Kasperek <joastr1@gmail.com>
2019-01-10 13:16:17 +0100

    add title component

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import uuid from 'uuid';
 import style from './App.css';
+import Title from '../components/Title';
 
 class App extends React.Component {
     constructor(props){
@@ -25,7 +26,7 @@ class App extends React.Component {
     render() {
         return (
             <div className={style.TodoApp}>
-                Tutaj pojawią się komponenty naszej aplikacji.
+                <Title title='Todo App' length={this.state.data.length} />
             </div>
         );
     }
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader",
                 options: {
-                    presets: ['es2015', 'react']
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
                 }
             },
             {
